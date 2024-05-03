90620dcfd7d78918c25a79ca9ec3885408a7c434
Israel CHIZUNGU <chizunguisrael@gmail.com>
2024-05-03 11:13:45 +0200

    Fixing routing issue

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -40,50 +40,11 @@ const items: MenuItem[] = [
     icon: <UserSwitchOutlined />,
   },
   {
-    key: 'tallylform',
+    key: 'tallyform',
     label: 'Tally forms',
     icon: <FormOutlined />,
   },
-  // {
-  //   key: 'sub2',
-  //   label: 'Navigation Two',
-  //   icon: <AppstoreOutlined />,
-  //   children: [
-  //     { key: '5', label: 'Option 5' },
-  //     { key: '6', label: 'Option 6' },
-  //     {
-  //       key: 'sub3',
-  //       label: 'Submenu',
-  //       children: [
-  //         { key: '7', label: 'Option 7' },
-  //         { key: '8', label: 'Option 8' },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   type: 'divider',
-  // },
-  // {
-  //   key: 'sub4',
-  //   label: 'Navigation Three',
-  //   icon: <SettingOutlined />,
-  //   children: [
-  //     { key: '9', label: 'Option 9' },
-  //     { key: '10', label: 'Option 10' },
-  //     { key: '11', label: 'Option 11' },
-  //     { key: '12', label: 'Option 12' },
-  //   ],
-  // },
-  // {
-  //   key: 'grp',
-  //   label: 'Group',
-  //   type: 'group',
-  //   children: [
-  //     { key: '13', label: 'Option 13' },
-  //     { key: '14', label: 'Option 14' },
-  //   ],
-  // },
+
 ];
 
 
@@ -95,7 +56,7 @@ const Sidebar: React.FC = () => {
   const onClick: MenuProps['onClick'] = (e) => { router.push(`/${e.key}`)  };
 
   return (
-    <div className='flex flex-col h-full bg-green-100'>
+    <div className='flex flex-col h-[100%] bg-green-100'>
       <div className='flex justify-center items-center mb-4 mt-4'>
         <Image src={'/images/directed-logo.png'} width={90} height={75} alt='DirectED logo' />
       </div>
